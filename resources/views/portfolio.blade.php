2c8c175cb57055d1c37fcac527e9273c41fe0bf1
oreneses <oreneses.g@gmail.com>
2022-10-20 16:57:23 +0200

    control structures with blade

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -4,4 +4,11 @@
 
 @section('content')
     <h1> PORTFOLIO </h1>
+    <ul>
+        @forelse ($portfolio as $portfolioItem)
+            <li>{{ $portfolioItem['title'] }}</li>
+        @empty
+            <p>No hay proyectos para mostrar</p>
+        @endforelse
+    </ul>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ use Illuminate\Support\Facades\Route;
 //     return view('home', compact('nombre'));
 // })->name('home');
 
+
 Route::view('/','home')->name('home');
 Route::view('/about','about')->name('about');
-Route::view('/portfolio','portfolio')->name('portfolio');
+Route::view('/portfolio','portfolio', compact('portfolio'))->name('portfolio');
 Route::view('/contact','contact')->name('contact');
