8ed4c4a8869bf0e2edc158cba35e9bec69dba86f
oreneses <oreneses.g@gmail.com>
2022-10-25 20:25:09 +0200

    validation form

--- app/Http/Controllers/ContactFormController.php
+++ app/Http/Controllers/ContactFormController.php
@@ -6,6 +6,13 @@ namespace App\Http\Controllers;
 class ContactFormController extends Controller
 {
     public function store(){
-        return request('name');
+        request()->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'subject' => 'required',
+            'content' => 'required|min:3'
+        ]);
+        
+        return 'Datos validados';
     }
 }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,10 +6,14 @@
     <h1> CONTACT </h1>
     <form action="{{route('contactForm')}}" method="post">
         @csrf
-        <input name="name" placeholder="Nombre" value="Oriol"><br>
-        <input name="email" type="email" placeholder="Email" value="oriol@hotmail.com"><br>
-        <input name="subject" placeholder="Asunto" value="Aprendiendo formularios"><br>
-        <textarea name="content" placeholder="Mensaje">Mensaje de prueba</textarea><br>
+        <input name="name" placeholder="Nombre" value="{{ old('name')}}"><br>
+        {!! $errors->first('name', '<small>:message</small><br>') !!}
+        <input name="email" type="email" placeholder="Email" value="{{ old('email')}}"><br>
+        {!! $errors->first('email', '<small>:message</small><br>') !!}
+        <input name="subject" placeholder="Asunto" value="{{ old('subject')}}"><br>
+        {!! $errors->first('subject', '<small>:message</small><br>') !!}
+        <textarea name="content" placeholder="Mensaje" value="{{ old('content')}}"></textarea><br>
+        {!! $errors->first('content', '<small>:message</small><br>') !!}
         <button type="submit">Enviar</button>
     </form>
 @endsection
