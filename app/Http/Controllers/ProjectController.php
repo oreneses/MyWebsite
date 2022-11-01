f13e611310fa9ee51e4376d5a317afa6de2e287e
oreneses <oreneses.g@gmail.com>
2022-11-01 20:58:37 +0100

    FormRequest implementation

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,35 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Http\Requests\CreateProjectRequest;
 
 class ProjectController extends Controller
 {
+    // Obtenemos proyectos
     public function index(){
         $projects = Project::get();
         return view('projects.index', compact('projects'));
     }
 
+    // Mostramos los detalles de un proyecto especifico
     public function show(Project $project){
         return view('projects.show', ['project'=>$project]);
     }
 
+    // Creamos proyectos
     public function create(){
         return view('projects.create');
     }
 
-    public function store(){
-        $title = request('title');
-        $description = request('description');
-
-        $url = request('title');
-        $urlFinal = strtolower(str_replace(' ', '-', $url));
-
-        Project::create([
-            'title' => $title,
-            'url' => $urlFinal,
-            'description' => $description
-        ]);
-
+    // Almacenamos proyectos (FormRequest)
+    public function store(CreateProjectRequest $request){
+        // Es importante el validated() de la request ya que sino podrian modificar la base de datos desde inspeccionar.
+        // El validated es necesario porque hemos desprotegido la asignacion masiva desde el modelo con $guarded = []
+        Project::create($request->validated());
         return redirect()->route('projects.index');
     }
     
--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,9 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    // Hemos de agregar esta propiedad para permitir asignacion masiva.  
-    protected $fillable = ['title', 'url', 'description'];
+    // Hemos de agregar esta propiedad para desproteger la asignacion masiva.
+    // Esto se utiliza porque si hubiesen muchos campos, tendriamos que agregar a $fillable todos los campos del formulario.
+    // De esta manera, desde el RequestForm permitimos que los campos que se envien a traves de la request sean los validados.  
+    protected $guarded = [];
 
+    // Sirve para mostrar la url con el campo URL de nuestra base de datos (por defecto utiliza la PK)
     public function getRouteKeyName()
     {
         return 'url';
